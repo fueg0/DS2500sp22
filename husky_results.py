7c9901a9f7cde65f645d5e18121b0bedbb438bad
Nick Fiorito <64492580+fueg0@users.noreply.github.com>
2022-01-27 18:01:50 -0500

    final version i think

--- husky_results.py
+++ husky_results.py
@@ -12,7 +12,7 @@ from collections import Counter
 
 # GLOBAL VARIABLES
 fig, ax = plt.subplots()
-fig, bx = plt.subplots()
+fig2, bx = plt.subplots()
 
 
 # FUNCTIONS
@@ -41,6 +41,7 @@ def transform_hockey(string_list, column):
 def clean_data(nested_dict, selected_fields):
     nested_dict = remove_keys(nested_dict, selected_fields, keep=True)
 
+    # make sure types are correct
     for keys, values in nested_dict.items():
         for nested_key in values:
             # Dynamic type-casting, if string CAN BE CAST to int then cast, else pass on exception and leave un-cast
@@ -48,7 +49,7 @@ def clean_data(nested_dict, selected_fields):
             try:
                 nested_dict[keys][nested_key] = int(nested_dict[keys][nested_key])
             except ValueError:
-                # ignore the exception
+                # ignore the exception, leave string intact
                 pass
 
     return nested_dict
@@ -70,13 +71,12 @@ def max_nested(nested_dict, selected_field):
 ###########
 # Helper Functions
 ###########
-
-
-def double_list_to_dict(data, fields):
+# Turns the two lists supplied into a dictionary keyed by the contents of fields
+def double_list_to_dict(values, keys):
     return_dict = {}
 
-    for i in range(len(fields)):
-        return_dict[fields[i]] = data[i]
+    for i in range(len(keys)):
+        return_dict[keys[i]] = values[i]
 
     return return_dict
 
@@ -96,6 +96,7 @@ def csv_to_2DList(filename):
     return file
 
 
+# Removes all keys from a nested dictionary, changing keep to True keeps only the given keys
 def remove_keys(nested_dict, keys_to_remove, keep=False):
     for keys, values in nested_dict.items():
         if keep:
@@ -122,8 +123,8 @@ hockey_dict = transform_hockey(file_data, 1)
 
 cleaned_data = clean_data(hockey_dict, ["Opponent", "W/L", "G", "A", "PP", "BLK"])
 
-# Perform Operations:
 
+# Perform Operations:
 ##########
 # On what date did we: score the most goals?
 #
@@ -133,8 +134,8 @@ for goals_key, goal_values in cleaned_data.items():
     if goal_values["G"] > max_goals:
         max_goals = goal_values["G"]
         max_goals_date = goals_key
-
-print("We scored the most goals (%d goals!) on %s" % (max_goals, max_goals_date))
+print("--------------------------")
+print("We scored the most goals (%d goals!) on %s\n" % (max_goals, max_goals_date))
 
 ##########
 # On what date did we: have the most assists?
@@ -145,8 +146,8 @@ for assist_key, assist_values in cleaned_data.items():
     if assist_values["A"] > max_assists:
         max_assists = assist_values["A"]
         max_assists_date = assist_key
-
-print("We got the most assists (%d assists!) on %s" % (max_assists, max_assists_date))
+print("--------------------------")
+print("We got the most assists (%d assists!) on %s\n" % (max_assists, max_assists_date))
 
 ##########
 # On what date did we: block the most shots?
@@ -157,8 +158,8 @@ for block_key, block_values in cleaned_data.items():
     if block_values["BLK"] > max_blocks:
         max_blocks = block_values["BLK"]
         max_blocks_date = block_key
-
-print("We got the most blocks (%d blocks!) on %s" % (max_blocks, max_blocks_date))
+print("--------------------------")
+print("We got the most blocks (%d blocks!) on %s\n" % (max_blocks, max_blocks_date))
 
 ##########
 # On how many dates did we: win?
@@ -167,8 +168,8 @@ win_counter = 0
 for win_key, win_values in cleaned_data.items():
     if win_values["W/L"] == "W":
         win_counter += 1
-
-print("We won on %d dates this year!" % win_counter)
+print("--------------------------")
+print("We won on %d dates this year!\n" % win_counter)
 
 ##########
 # On how many dates did we: have exactly one power play?
@@ -177,37 +178,58 @@ pp_counter = 0
 for pp_key, pp_values in cleaned_data.items():
     if pp_values["PP"] == 1:
         pp_counter += 1
+print("--------------------------")
+print("We had exactly one power play on %d dates this year!\n" % pp_counter)
 
-print("We had exactly one power play on %d dates this year!" % pp_counter)
 
 ###########
 # HW Visualizations
 ###########
-
 # A histogram showing the number of games in which we scored 0, 1..., n goals.
-cleaned_for_goals = remove_keys(cleaned_data.copy(), ["G"], keep=True)
-all_goals = []
+def plot_first(data):
+    all_goals = []
+    plt.title("Goal Scoring Frequencies from Northeastern Women's Hockey")
+    plt.xlabel("Number of Goals Scored")
+    plt.ylabel("Number of Times This Many Goals Was Scored")
+    plt.grid(visible=True, alpha=0.5)
 
-for goal_data in cleaned_for_goals.values():
-    for goal_count in goal_data.values():
-        all_goals.append(goal_count)
+    for goal_data in data.values():
+        for goal_count in goal_data.values():
+            all_goals.append(goal_count)
 
-# https://www.geeksforgeeks.org/python-find-most-frequent-element-in-a-list/
-mode_goal = Counter(all_goals).most_common(1)[0][1]
+    # https://www.geeksforgeeks.org/python-find-most-frequent-element-in-a-list/
+    mode_goal = Counter(all_goals).most_common(1)[0][1]
 
-ax.set(xlim=(0, max_goals), xticks=np.arange(0, (max_goals + 2)),
-       ylim=(0, mode_goal), yticks=np.arange(0, mode_goal, 2))
+    ax.set(xlim=(0, max_goals), xticks=np.arange(0, (max_goals + 2)),
+           ylim=(0, mode_goal), yticks=np.arange(0, mode_goal, 2))
 
-ax.hist(all_goals, bins=(max_goals + 1), align='mid', range=[0, (max_goals + 1)], linewidth=0.5, edgecolor="black")
-# TODO: Add labeling to ax
-# TODO: Add grid https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.grid.html#
+    ax.hist(all_goals, bins=(max_goals + 1), align='mid', range=[0, (max_goals + 1)], linewidth=0.5, edgecolor="black")
+
 
 # A scatter plot of shots blocked on the x-axis vs. assists on the y-axis.
+def plot_second(data):
+    plt.title("Shots Blocked vs. Assists from Northeastern Women's Hockey")
+    plt.xlabel("Shots Blocked in Game")
+    plt.ylabel("Assists Made in Game")
+    plt.grid(visible=True, alpha=0.5)
 
-cleaned_for_plot = remove_keys(cleaned_data.copy(), ["A", "BLK"], keep=True)
-for stats in cleaned_for_plot.values():
-    bx.scatter(stats["BLK"], stats["A"], color="black")
-# TODO: Add labeling to bx
-# TODO: Add grid, this is illegible
+    for stats in data.values():
+        bx.scatter(stats["BLK"], stats["A"], color="green")
 
-plt.show()
+
+def main():
+    cleaned_for_goals = remove_keys(cleaned_data.copy(), ["G"], keep=True)
+    cleaned_for_plot = remove_keys(cleaned_data.copy(), ["A", "BLK"], keep=True)
+
+    plt.figure(1)
+    plot_first(cleaned_for_goals)
+
+    plt.figure(2)
+    plot_second(cleaned_for_plot)
+    plt.show()
+
+    exit(0)
+
+
+if __name__ == '__main__':
+    main()
